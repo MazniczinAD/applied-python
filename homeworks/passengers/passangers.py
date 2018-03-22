420e686e60ea9e934c208a64ce1938ed3745758d
ahtoh1523@gmail.com <ahtoh1523@gmail.com>
2018-03-22 18:31:20 +0300

    home work 1

--- homeworks/passengers/passangers.py
+++ homeworks/passengers/passangers.py
@@ -1,13 +1,71 @@
 # -*- encoding: utf-8 -*-
 
 
-def process(data, events, car):
+def process(trains, events, car):
     '''
         ТУТ ДОЛЖЕН БЫТЬ ВАШ КОД
     '''
-    for train in data:
-        print(train['name'])
-        for car in train['cars']:
-            print('\t{}'.format(car['name']))
-            for man in car['people']:
-                print('\t\t{}'.format(man))
+
+    event_log(events)
+    trains_log(trains)
+    for event in events:
+        prov=event_creator(event,trains)
+        if prov==-1:
+            return -1
+    for train in trains:
+        for car_of_function in train['cars']:
+            if car==car_of_function['name']:
+                result=len(car_of_function['people'])
+    return result
+
+def event_creator(event, trains):
+    if event["type"]=="walk":
+        for train in trains:
+            for car in train['cars']:
+                if 0!=car["people"].count(event["passenger"]):
+                    car_number = train['cars'].index(car)+int(event['distance'])
+                    if (car_number<0)or((len(train['cars'])-1)<car_number):
+                        return -1
+                    else:
+                        name = event['passenger']
+                        train['cars'][car_number]["people"].append(name)
+                        train['cars'][train['cars'].index(car)]["people"].remove(event['passenger'])
+                        return 1
+    elif event["type"]=="switch":
+        s=list()
+        for train in trains:
+            if train['name'] == event['train_from']:
+                if len(train['cars'])<event['cars']:
+                    return -1
+                else:
+                    for i in range(event['cars']):
+                        test_iterator = event['cars']-i
+                        s.append(train['cars'][len(train['cars']) - test_iterator])
+                        del (train['cars'][len(train['cars']) - test_iterator])
+        for train in trains:
+            if train['name']== event['train_to']:
+                train['cars'].extend(s)
+        return 1
+    else:
+        print("error")
+    print("end of change function")
+
+def event_log(events):
+    print("События:\n")
+    for event in events:
+        if event['type']=='walk':
+            print('\tПасажир:',event['passenger'])
+            print('\tЧто делает:', event['type'])
+            print('\tРасстояние:', event['distance'],'\n')
+        else:
+            print('\tКоличество ваглнов', event['cars'])
+            print('\tОткуда', event['train_from'])
+            print('\tЧто происходит', event['type'])
+            print('\tкуда', event['train_to'], '\n')
+def trains_log (trains):
+    for train in trains:
+        print("поезд -", train['name'])
+        for car_of_function in train['cars']:
+            print("\tВагон -", car_of_function['name'])
+            for man in car_of_function['people']:
+                print("\t\tПасажир -", man)
--- homeworks/passengers/test.py
+++ homeworks/passengers/test.py
@@ -9,11 +9,16 @@ error_message = 'ERROR in file {}. Expected: "{}", got: "{}"'
 
 
 def run_tests():
+    i=0
     for filename in glob('tests/*.json'):
+        i=i+1
         data = json.load(open(filename))
         trains, events, result = data['trains'], data['events'], data['result']
+        print('test №', i, '\n', trains, '\n', events)
         got = process(trains, events, result['car'])
         expected = result['amount']
+        print("expected: ",result)
+        print(trains)
         if got != expected:
             print(error_message.format(filename, expected, got))
             return
